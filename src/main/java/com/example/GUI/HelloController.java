bc7c5c98150d29aa1feda88194a29b53c85df920
byrkedavid <byrke.david@gmail.com>
2023-11-27 21:25:32 -0500

    Loads login page, then switches to main scene

--- src/main/java/com/example/GUI/HelloController.java
+++ src/main/java/com/example/GUI/HelloController.java
@@ -1,10 +1,17 @@
 package com.example.GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 
@@ -20,18 +27,26 @@ public class HelloController {
 
     @FXML
     private TextField budgetIn;
-
     @FXML
     private TextField depositIn;
-
     @FXML
     private TextField expenseIn;
-
     @FXML
     private Text monthlyBudgetOut;
-
     @FXML
     private Text remainingBudgetOut;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("main-page.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     void addDeposit(MouseEvent event) {
